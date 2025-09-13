e6bdc0a28f2500d7b4cd674b41744bb96d4aa9bc
Christian Johansen <Christian.Johansen@uppsalabiobank.uu.se>
2025-09-13 02:21:31 +0200

    Fixed restarts and screen resize issue

--- xstartup.sh
+++ xstartup.sh
@@ -6,11 +6,22 @@ unset DBUS_SESSION_BUS_ADDRESS
 # Start the window manager in the background
 openbox &
 
-# Start the main application in the foreground
-inkcut &
+while true
+do
+  # Start the main application in the foreground
+  DISPLAY=:1 inkcut &
 
-sleep 5
+  echo "Waiting for Inkcut window..."
+  for i in {1..100}; do # Timeout after 10 seconds (100 * 0.1s)
+      # Check if a window with "Inkcut" in the title exists
+      if DISPLAY=:1 wmctrl -l | grep -q "Inkcut"; then
+          echo "Inkcut window found. Maximizing."
+          DISPLAY=:1 wmctrl -r Inkcut -b add,maximized_vert,maximized_horz
+          break # Exit the loop once the window is found and maximized
+      fi
+      sleep 0.1
+  done
 
-DISPLAY=:1 wmctrl -r Inkcut -b add,maximized_vert,maximized_horz
-
-wait
+  wait $!
+  sleep 1
+done
